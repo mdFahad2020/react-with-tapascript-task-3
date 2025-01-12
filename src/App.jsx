cfecd687aa906ffdd7ba19e6594698d39b926177
fahad <mdabdulfahad2020@gmail.com>
2025-01-12 10:50:28 +0600

    [ADD] task-1

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <h1 className="text-xl text-green-500 text-center p-5">We are here to learn React from tapaScript</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Banner.jsx
@@ -0,0 +1,9 @@
+
+
+const Banner = () => {
+  return (
+    <div className="bg-gray-500 p-4 rounded-md border text-white">Banner</div>
+  )
+}
+
+export default Banner
